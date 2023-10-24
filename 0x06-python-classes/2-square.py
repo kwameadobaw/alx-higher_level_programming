a6aa4f569e251923bf62f430ee510709a01761c2
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-10-24 13:18:12 +0000

    Task 3 completed

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+class Square:
+    """Represents a new square"""
+
+    def __init__(self, size=0):
+
+        """Initializes a new square
+
+        Args:
+            size (int): The size of the square
+
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
