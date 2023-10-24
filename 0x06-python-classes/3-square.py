da8eaa359d2414d09a2ddc7f62d451257afcae98
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-10-24 21:43:23 +0000

    Fixing pycode

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,17 +6,25 @@ class Square:
     """Represents a square"""
 
     def __init__(self, size=0):
-        """Initializes a new square
-
+        """Initializing this square class
         Args:
-            size (int): The size of the square
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
         """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = size
 
     def area(self):
-        """Return the area of the square""" 
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
+
         return (self.__size ** 2)
