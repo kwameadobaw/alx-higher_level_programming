8b2a071500c4c78c7ae8b16159201d22219dfabd
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-10-24 12:55:13 +0000

    Task 1 done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines a square"""
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size)
+    """Initializes a square
+
+    Args:
+        size: The size of the initialized square
+    """
+
+    size = self.__size
