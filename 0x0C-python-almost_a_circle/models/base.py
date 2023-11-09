50575b3bac22a6ba846c8563a2f120433a0484e3
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-11-09 12:34:30 +0000

    Trying to fix errors

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Deines a base Model class"""
+
+
+class Base:
+    """Represents a base odel"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""Defines a rectangle class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Represents a rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+            """
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+        @property
+        def width(self):
+            return (self.__width)
+
+        @width.setter
+        def width(self, value):
+            if (type(value) is not int):
+                raise TypeError("width must be an integer")
+            if width <= 0:
+                raise ValueError("width must be > 0")
+            self.__width = value
+
+        @property
+        def height(self):
+            return (self.height)
+
+        @height.setter
+        def height(self, value):
+            if (type(value) != int):
+                raise TypeError("height must be an integer")
+            if width <= 0:
+                raise ValueError("height must be > 0")
+            self.__height = value
+
+        @property
+        def x(self):
+            return (self.__x)
+
+        @x.setter
+        def x(self, value):
+            if type(value) is not int:
+                raise TypeError("x must be an integer")
+            if x < 0:
+                raise ValueError("x must be >= 0")
+            self.__x = value
+
+        @property
+        def y(self):
+            return (self.__y)
+
+        @y.setter
+        def y(self, value):
+            if type(value) is not int:
+                raise TypeError("y must be an integer")
+            if y < 0:
+                raise ValueError("y must be >= 0")
+            self.__y = value
+
+        def area(self):
+            """Defines the area of the rectangle"""
+            return (self.__width * self.__height)
