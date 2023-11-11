d2a9d87e2574f66d8f18763714caf6b6a328be2f
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-11-11 20:06:46 +0000

    First advanced task done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Deines a base Model class"""
 import json
+import csv
 
 
 class Base:
@@ -57,4 +58,47 @@ class Base:
                 list_dicts = Base.from_json_string(f.read())
                 return [cls.create(**d) for d in list_dicts]
         except IOError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Write the CSV serialization of a list of objects to a file.
+
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
+        filename = f"{cls.__name__}.csv"
+
+        with open(filename, "w", newline="") as csvfile:
+            fieldnames = ["id", "width", "height", "x", "y"]
+            \if cls.__name__ == "Rectangle" else ["id", "size", "x", "y"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            if list_objs:
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+            else:
+                csvfile.write("[]")
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Return a list of classes instantiated from a CSV file.
+
+        Reads from `<cls.__name__>.csv`.
+
+        Returns:
+            If the file does not exist - an empty list.
+            Otherwise - a list of instantiated classes.
+        """
+        filename = f"{cls.__name__}.csv"
+
+        try:
+            with open(filename, "r", newline="") as csvfile:
+                fieldnames = ["id", "width", "height", "x", "y"]
+                \if cls.__name__ == "Rectangle" else ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dicts = [dict([k, int(v)] for k, v in d.items())
+                              for d in list_dicts]
+                return [cls.create(**d) for d in list_dicts]
+        except FileNotFoundError:
             return []
