74eca82f5f1d43dbfa2cbbcb3b5b4fbdc47cd5ed
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2024-01-09 12:05:46 +0000

    Added new

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
@@ -9,7 +9,7 @@ class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width))
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 }
 module.exports = Rectangle;
