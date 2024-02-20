f39fed97a4bb44f7e51a59d50c1d66301ea04161
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2024-02-20 19:57:39 +0000

    Task 0 and 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], { encoding: 'utf-8' }, (err) => {
-  if (err) {
-    console.error(`An error occurred: ${err}`);
-  } else {
-    console.log('Content has been written to the file successfully.');
-  }
+fs.writeFile(process.argv[2], process.argv[3], { encoding: 'utf-8' }, error => {
+  if (error) console.log(error);
 });
